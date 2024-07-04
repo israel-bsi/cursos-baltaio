bd4f8948054edfca426e1272e5a3534a60bec0d7
israel-bsi <masterisraelsilva@gmail.com>
2024-07-03 23:17:32 -0300

    [Fullstack .NET] Finalizado Apresenttação do curso, Estruturando o Projeto e Fundamento dos Minimal APIs

---
+++ Dima/Dima.Api/Program.cs
@@ -0,0 +1,37 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapPut(
+    "/v1/transactions",
+    (Request request, Handler handler) => handler.Handle(request))
+    .WithName("Transactions: Create")
+    .WithSummary("Cria uma nova transação")
+    .Produces<Response>();
+
+app.Run();
+
+//Request
+public class Request
+{
+    public string Title { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public int Type { get; set; }
+    public decimal Amount { get; set; }
+    public long CategoryId { get; set; }
+    public string UserId { get; set; } = string.Empty;
+}
+
+//Response
+public class Response
+{
+    public long Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+}
+
+public class Handler
+{
+    public Response Handle(Request request)
+    {
+        return new Response { Id = 1, Title = request.Title };
+    }
+}
