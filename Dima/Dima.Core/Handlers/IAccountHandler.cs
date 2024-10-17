afea7630f5aea542796c5b4c7360617b9d5b0a26
israel-bsi <masterisraelsilva@gmail.com>
2024-10-17 17:02:00 -0300

    Adicionado configurações do frontend

---
+++ Dima/Dima.Core/Handlers/IAccountHandler.cs
@@ -0,0 +1,11 @@
+﻿using Dima.Core.Request.Account;
+using Dima.Core.Response;
+
+namespace Dima.Core.Handlers;
+
+public interface IAccountHandler
+{
+    Task<Response<string>> LoginAsync(LoginRequest request);
+    Task<Response<string>> RegisterAsync(RegisterRequest request);
+    Task LogoutAsync();
+}
