4d93e98989e179977c4ccf570e5cc48dcfc68a0f
israel-bsi <masterisraelsilva@gmail.com>
2024-11-03 23:27:03 -0300

    Adição do curso de fundamentos do blazor com .net 8

---
+++ BlazorShop/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorShop.Models;
+
+public class Category
+{
+    [Key]
+    public int Id { get; set; }
+    [Required(ErrorMessage = "Informe o título")]
+    [MinLength(3, ErrorMessage = "A categoria deve ter pelo menos 3 caracteres")]
+    [MaxLength(120, ErrorMessage = "A categoria deve ter no máximo 120 caracteres")]
+    public string Title { get; set; } = string.Empty;
+}
